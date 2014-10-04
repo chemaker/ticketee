55888a0e9e5aa4f1d25f8095670bdc309a414d08
Chema Álvarez <chema@codevince.com>
2014-10-04 22:10:39 +0200

    Implement creating tickets for a project

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   root "projects#index" 
 
-  resources :projects
+  resources :projects do
+    resources :tickets 
+  end
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
