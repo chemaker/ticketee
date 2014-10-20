22bb050c4228964b717a97016dffc57eb36f2f0e
Chema Álvarez <chema@codevince.com>
2014-10-20 23:49:32 +0200

    Added admin flag to user.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,10 +1,14 @@
 FactoryGirl.define do
 
-  factory :user do
-    name "Example user"
-    email "sample@example.com"
-    password "blablabla"
-    password_confirmation "blablabla"
-  end
-  
+factory :user do
+	name "Example user"
+	email "sample@example.com"
+	password "blablabla"
+	password_confirmation "blablabla"
+
+  	factory :admin_user do
+      	admin true
+	end
+end
+
 end
